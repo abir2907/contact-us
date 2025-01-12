08d4d3e867ca597450d5fc22a02c034f16ba28c9
Abir Chodha <abirchodha1@gmail.com>
2025-01-12 18:22:29 +0530

    Add contact us section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ConactHeader from "./components/ContactHeader/ConactHeader";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <div>
       <Navigation />
+      <ConactHeader />
     </div>
   );
 }
