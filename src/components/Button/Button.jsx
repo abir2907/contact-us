612ad7290dff85854aec5653c98a38e199b70f76
Abir Chodha <abirchodha1@gmail.com>
2025-01-12 19:20:27 +0530

    Fix props validation error in Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import styles from "./Button.module.css";
+import PropTypes from "prop-types";
 
 const Button = (props) => {
   return (
@@ -7,6 +8,11 @@ const Button = (props) => {
       {props.text}
     </button>
   );
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
 };
 
 export default Button;
